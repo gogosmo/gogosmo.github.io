9fe781da1b19435825986930d69a2a262b3afbe4
Jughurta Gahlouz <jugogahlouz@gmail.com>
2023-11-14 12:48:55 +0100

    toto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Jughurta-Gahlouz.github.io',
+    site: 'https://Gogo-IGM-BK.github.io',
 });
